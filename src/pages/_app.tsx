ff594f6bc2752a0e51404b9bea46bf54d820ea06
Matt Dean <mdean400@gmail.com>
2022-09-17 10:39:28 -0600

    Fix a type

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
 // src/pages/_app.tsx
-import "../styles/globals.css";
+import type { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-import type { AppType } from "next/dist/shared/lib/utils";
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
 import { trpc } from "../utils/trpc";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
+const MyApp = ({ Component, pageProps }: AppProps<{ session: Session }>) => {
   return (
     <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
